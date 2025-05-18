ac2b31282f0f13f8562f2c743e0d2764431e7896
Andrey Baluevsky <AndreyBaluevsky@gmail.com>
2025-05-18 13:16:16 +0300

    2025-05-17 Alpha-release distrib

---
+++ Bomber/distrib/1.0.0a/runBomberJar.cmd
@@ -0,0 +1,3 @@
+SET IDEACOM=C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2020.1.4\
+SET java_exe="%IDEACOM%jbr\bin\java.exe"
+%java_exe% -Dfile.encoding=UTF-8 -classpath bomber-1.0.0a.jar Main
